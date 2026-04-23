145bbb9f40d67fe271dde42511038162ed98ef1d
Lewis Blackburn <lewisblackburn9@gmail.com>
2026-04-23 23:13:32 +0100

    Amended textarea component again

--- resources/views/components/text-textarea.blade.php
+++ resources/views/components/text-textarea.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<textarea @disabled($disabled) {{ $attributes->merge(['class' => 'bg-[--color-background] border-[--color-border] text-[--color-text] placeholder:text-[--color-surface-alt] focus:border-[--color-primary] focus:ring-[--color-primary] rounded-md shadow-sm']) }}>{{  old($attributes->get('name'))  }}</textarea>
+<textarea @disabled($disabled) {{ $attributes->merge(['class' => 'bg-[--color-background] border-[--color-border] text-[--color-text] placeholder:text-[--color-surface-alt] focus:border-[--color-primary] focus:ring-[--color-primary] rounded-md shadow-sm']) }}>{{ old($attributes->get('name')) }}</textarea>
