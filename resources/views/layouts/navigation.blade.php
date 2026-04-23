225775b38aea8eb22466478e8eb9bc1587158c5f
Lewis Blackburn <lewisblackburn9@gmail.com>
2026-04-23 01:59:38 +0100

    Added annual leave to navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('leave.view')" :active="request()->routeIs('leave.view')">
+                        {{ __('Annual Leave') }}
+                    </x-nav-link>
                 </div>
             </div>
 
