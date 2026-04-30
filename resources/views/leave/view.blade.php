8d5a0749832c0ccbc42131319a40475262b0f4ef
Lewis Blackburn <lewisblackburn9@gmail.com>
2026-04-30 14:21:07 +0100

    Added create new leave request button to bottom of your leave table

--- resources/views/leave/view.blade.php
+++ resources/views/leave/view.blade.php
@@ -63,6 +63,16 @@
                         </tr>
                     @endforeach
 
+                    <tr class="hover:bg-[var(--color-surface-alt)] transition">
+                        <td colspan="5" class="text-center text-sm text-[var(--color-subtletext)]">
+                            <a href="{{ route('leave.form') }}" class="inline-flex items-center justify-center cursor-pointer w-full gap-2 px-6 py-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
+
+                                Create new leave request
+                            </a>
+                        </td>
+                    </tr>
+
                 </tbody>
             </table>
         </div>
