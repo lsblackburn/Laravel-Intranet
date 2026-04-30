8257d321265631b813815768e8bcb667571dd96f
Lewis Blackburn <lewisblackburn9@gmail.com>
2026-04-30 19:04:27 +0100

    Removed verified from protected middleware

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth'])->group(function () {
 
     Route::get('/dashboard', function () {
         return view('dashboard');
