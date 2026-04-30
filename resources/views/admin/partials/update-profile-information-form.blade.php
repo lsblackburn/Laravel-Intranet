3380e4097311cb3fdc78f1dc1d7640100c742910
Lewis Blackburn <lewisblackburn9@gmail.com>
2026-04-30 10:31:00 +0100

    Removed if statement for verification email from admin update profile partial

--- resources/views/admin/partials/update-profile-information-form.blade.php
+++ resources/views/admin/partials/update-profile-information-form.blade.php
@@ -5,13 +5,9 @@
         </h2>
 
         <p class="mt-1 text-sm text-[--color-subtletext]">
-            {{ __("Update your account's profile information and email address.") }}
+            {{ __("Update this user's profile information and email address.") }}
         </p>
     </header>
-
-    <form id="send-verification" method="post" action="{{ route('verification.send') }}">
-        @csrf
-    </form>
 
     <form method="post" action="{{ route('admin.users.update', $user->id) }}" class="mt-6 space-y-6">
         @csrf
@@ -27,24 +23,6 @@
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
             <x-input-error class="mt-2" :messages="$errors->get('email')" />
-
-            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
-                <div>
-                    <p class="text-sm mt-2 text-[--color-subtletext]">
-                        {{ __('Your email address is unverified.') }}
-
-                        <button form="send-verification" class="underline text-sm text-[--color-subtletext] hover:text-[--color-text] rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[--color-primary]">
-                            {{ __('Click here to re-send the verification email.') }}
-                        </button>
-                    </p>
-
-                    @if (session('status') === 'verification-link-sent')
-                        <p class="mt-2 font-medium text-sm text-[--color-success-text]">
-                            {{ __('A new verification link has been sent to your email address.') }}
-                        </p>
-                    @endif
-                </div>
-            @endif
         </div>
 
         <div class="flex items-center gap-4">
