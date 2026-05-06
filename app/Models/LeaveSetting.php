f8ac8390c1aedbe924950a5f5a3ecf164b3f7ad4
Lewis Blackburn <lewisblackburn9@gmail.com>
2026-05-06 14:44:23 +0100

    Fillables for LeaveSetting model

--- app/Models/LeaveSetting.php
+++ app/Models/LeaveSetting.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class LeaveSetting extends Model
 {
-    //
+    protected $fillable = [
+        'base_allowance',
+        'increase_after_years',
+        'increase_by_days',
+        'maximum_allowance',
+        'leave_refresh_day',
+        'leave_refresh_month',
+    ];
 }
