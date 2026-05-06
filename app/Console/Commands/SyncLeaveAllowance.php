c1937c891e72b364cdab78b920c8654b240ea2b3
Lewis Blackburn <lewisblackburn9@gmail.com>
2026-05-06 15:48:09 +0100

    Added scheduler reference

--- app/Console/Commands/SyncLeaveAllowance.php
+++ app/Console/Commands/SyncLeaveAllowance.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Attributes\Description;
 use Illuminate\Console\Attributes\Signature;
+use Illuminate\Support\Facades\Schedule;
 use Illuminate\Console\Command;
 use App\Models\LeaveSetting;
 use App\Models\User;
