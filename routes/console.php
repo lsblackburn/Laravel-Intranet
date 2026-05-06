fe0f7e2587d5c92c69ed30d7b81087f7089ec9af
Lewis Blackburn <lewisblackburn9@gmail.com>
2026-05-06 16:59:01 +0100

    Removed white space in console.php route

--- routes/console.php
+++ routes/console.php
@@ -8,5 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-
 Schedule::command('leave:sync-allowances')->daily();
